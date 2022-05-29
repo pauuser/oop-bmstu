fb5d2c467d12a72d1447413bd2739c70bc284ca7
Pavel Ivanov <80483515+pauuser@users.noreply.github.com>
2022-05-29 19:57:50 +0300

    initial commit

---
+++ lab_04/gui/mainwindow.h
@@ -0,0 +1,26 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QVBoxLayout>
+#include <memory>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+
+    std::unique_ptr<QVBoxLayout> layout_;
+    //Lift my_lift;
+};
+#endif // MAINWINDOW_H
